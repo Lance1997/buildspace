8cce038976acd74afdd67281aed6965208ddaafc
Sir Lancelot <lanarmah@gmail.com>
2021-12-03 16:52:50 +0000

    updated contract

--- web3-solidity-eth/scripts/run.js
+++ web3-solidity-eth/scripts/run.js
@@ -1,8 +1,21 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveContract.getTotalWaves();
+
+  waveTxn = await await waveContract.connect(randomPerson).waveAndSayHi();
+  await waveTxn.wait();
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
